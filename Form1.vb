8859534e89aa5d6cc1567bd822537373b3d0cf56
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 09:32:57 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+        n = TextBox2.Text
+        TextBox3.Text = Math.Round(Val(TextBox1.Text), n)
+    End Sub
+End Class
